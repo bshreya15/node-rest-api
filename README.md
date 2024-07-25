6d25fe8c2db99ec31d5ed68542f434d8b8343a2c
Aadarsh Sharan <aadarsh.tmp+gitlab@gmail.com>
2024-07-25 15:47:14 +0530

    feat. handle cors

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ REST - REpresentational State Transfer
 client sends request to server
 server responds with text/html, Google geolocation API, 
 JSON, XML, URLEncoded, FormData
+
+
+
+create local server using expressJS
+create endpoints
+create HTTP methods on those endpoints
+handle errors
+body-parser feature
+
+cors - cross origin resource sharing
+cors error - security error if client and server are not on the same machine
+add some headers to allow client to access the server
--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+//CORS
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin",'*');
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type,Accept,Authorization");
+
+    if(req.method ==='OPTIONS'){
+        res.header("Access-Control-Allow-Methods","PUT,POST,PATCH,DELETE,GET")
+        return res.status(200).json({})
+    }
+    next();
+})
+
 const productRoutes = require("./app/routes/products");
 const orderRoutes = require("./app/routes/orders");
 
