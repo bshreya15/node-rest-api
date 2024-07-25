03e871b3cffe0676b056eddacf0b366bff517986
Aadarsh Sharan <aadarsh.tmp+gitlab@gmail.com>
2024-07-25 15:10:09 +0530

    hot reloading nodemon, logging, err handling

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const productRoutes = require('./app/routes/products')
-const orderRoutes = require('./app/routes/orders')
+//logging package for nodeJS
+const morgan = require("morgan");
 
-app.use('/products',productRoutes);
-app.use('/orders',orderRoutes);
+app.use(morgan("dev"));
 
-app.use((req, res, next)=>{
-    res.status(200).json({
-        message:'It works! \n 1. /products'
-    })
-})
+const productRoutes = require("./app/routes/products");
+const orderRoutes = require("./app/routes/orders");
 
-module.exports =app;
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("not found");
+  error.status=404;
+  next(error);
+//   res.status(200).json({
+//     message: "It works! \n 1. /products",
+//   });
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
