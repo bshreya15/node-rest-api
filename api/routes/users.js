80c308619a3675c309fdaebccb0dbcaf3f546360
Shreya Bakshi <i.bakshi.shreya@gmail.com>
2024-08-02 15:30:38 +0530

    json web token signing

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const process = require("../../nodemon.json");
 
 const User = require("../models/user");
 // const Product = require("../models/product");
@@ -48,38 +51,51 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
-router.post("/login", (req,res,next)=>{
+router.post("/login", (req, res, next) => {
   User.find({
-    email: req.body.email
-  }).exec().then(user =>{
-    if(user.length < 1){
-      return res.status(401).json({
-        message:"Authentication failed"
-      })
-    }
-    bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
-      if(err){
+    email: req.body.email,
+  })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
         return res.status(401).json({
-          message:'Authentication failed'
-        })
+          message: "Authentication failed",
+        });
       }
-      if(result){
-        return res.status(200).json({
-          message:"Authentication successful"
-        })
-      }
-      res.status(401).json({
-        message:'Authentication failed'
-      })
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Authentication failed",
+          });
+        }
+        if (result) {
+         const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn:"1h"
+            }
+          );
+          return res.status(200).json({
+            message: "Authentication successful",
+            token:token
+          });
+        }
+        res.status(401).json({
+          message: "Authentication failed",
+        });
+      });
     })
-
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).josn({
-      error: err,
+    .catch((err) => {
+      console.log(err);
+      res.status(500).josn({
+        error: err,
+      });
     });
-  });
-})
+});
 
 router.delete("/:userId", (req, res, next) => {
   User.deleteOne({ _id: req.params.userId })
